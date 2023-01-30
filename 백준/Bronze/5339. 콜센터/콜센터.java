25d3f042af949784d71aff5b3806486b83c7c3bd
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-01-30 10:03:12 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13832 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]) {
+        //scan();
+        call();
+    }
+    public static void scan(){
+        Scanner sc = new Scanner(System.in);
+        String a, b;
+        a = sc.nextLine();
+        b = sc.nextLine();
+    }
+    public static void call(){
+        System.out.println("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
