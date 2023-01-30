7066e72861b6e6e6fb8441ad25223045933e0c7f
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-01-30 10:01:04 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]) {
+        //scan();
+        welcome();
+    }
+    public static void scan(){
+        Scanner sc = new Scanner(System.in);
+        String a, b;
+        a = sc.nextLine();
+        b = sc.nextLine();
+    }
+    public static void welcome(){
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
