f46402db47e1c6e1e46666b18228f4ba77fe8e45
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-01-30 10:02:03 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 120 ms, Memory: 13852 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]) {
+        //scan();
+        logo();
+    }
+    public static void scan(){
+        Scanner sc = new Scanner(System.in);
+        String a, b;
+        a = sc.nextLine();
+        b = sc.nextLine();
+    }
+    public static void logo(){
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
