aa940d26eaf72ef57cfb6a630e1b3be3ebf12544
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-02-01 09:46:59 +0900

    [Bronze III] Title: 최소, 최대, Time: 1844 ms, Memory: 334216 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,21 +1,6 @@
 import java.io.IOException;
 import java.util.*;
 public class Main{
-    public static class Node{
-        int o;
-        Node(int o){
-            this.o = o;
-        }
-    }
-
-    public static List<Node> list = new ArrayList<>();
-    //public static PriorityQueue<Integer> que = new PriorityQueue<>();
-    public static Comparator<Node> comparator = new Comparator<Node>() {
-        @Override
-        public int compare(Node o1, Node o2) {
-            return Integer.compare(o1.o, o2.o);
-        }
-    };
 
     public static void main(String args[]) throws IOException {
         scan();
@@ -23,16 +8,27 @@ public class Main{
     public static void scan(){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int max = 0;
+        int min = 0;
         List<Integer> result = new ArrayList<Integer>();
         for(int i = 0 ; i < n ; i ++){
             int m = sc.nextInt();
 
-            list.add(new Node(m));
+            result.add(m);
         }
-        Collections.sort(list, comparator);
+        max = result.get(0);
+        min = result.get(0);
+        for(int i = 0 ; i < result.size() ; i ++){
+            if(max < result.get(i)){
+                max = result.get(i);
+            }
+            if(min > result.get(i)){
+                min = result.get(i);
+            }
+        }
 
-        System.out.println(list.get(0).o);
-        System.out.println(list.get(list.size()-1).o);
+        System.out.println(min);
+        System.out.println(max);
         sc.close();
     }
 }
