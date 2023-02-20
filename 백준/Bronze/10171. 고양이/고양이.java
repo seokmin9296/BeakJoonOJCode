317f0a6a34cb3f59b774d5ec1c11877254853c1a
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-02-20 16:57:10 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+        scan();
+    }
+    public static void scan() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
