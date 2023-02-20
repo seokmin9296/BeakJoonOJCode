2644377a572b2f37648df9d9575e5726014440ff
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-02-20 11:26:22 +0900

    [Silver II] Title: 베르트랑 공준, Time: 252 ms, Memory: 27588 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main{
     public static void main(String args[]) throws IOException {
@@ -10,30 +11,29 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true){
             int n = Integer.parseInt(br.readLine());
-            if(n == 0) break;
-            countPrime(n);
+            if(n == 0)break;
+            isPrime(n);
         }
     }
 
-    public static void countPrime(int n) {
+    public static void isPrime(int n) {// 에라토스테네스의 체 방식
+        boolean[] prime = new boolean[(n*2)+1];
         int cnt = 0;
-        for(int i = n+1; i <= 2*n ; i++){
-            if(isPrime(i)){
-                cnt ++;
+        Arrays.fill(prime, true);
+        prime[0] = false;
+        prime[1] = false;
+        for( int i = 2 ; i <= Math.sqrt(n*2) ; i ++){
+            if(prime[i]){
+                for(int j = i * i ; j <= n*2 ; j += i){
+                    prime[j] = false;
+                }
+            }
+        }
+        for(int i = n+1 ; i <= n*2 ; i ++){
+            if(prime[i]){
+                cnt++;
             }
         }
         System.out.println(cnt);
-    }
-
-    public static boolean isPrime(int n){
-        if( n <= 1){
-            return false;
-        }
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if( n % i == 0){
-                return false;
-            }
-        }
-        return true;
     }
 }
