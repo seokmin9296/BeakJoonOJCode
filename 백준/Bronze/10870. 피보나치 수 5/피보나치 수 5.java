377f0daa54bd5f12ba7b50688d1f7276d336041f
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-02-21 13:28:24 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+        scan();
+    }
+    public static void scan() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibo(n));
+    }
+    public static int fibo(int n){
+        if(n == 0){
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+          return fibo(n-1)  + fibo(n - 2);
+        }
+    }
+}
