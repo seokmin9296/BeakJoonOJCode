502e1c8a8e31ef47162c0243a6fd359d0a70a594
seokmin9296 <116157738+seokmin9296@users.noreply.github.com>
2023-03-02 09:48:24 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 992 ms, Memory: 61280 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException {
+        scan();
+    }
+    public static void scan() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        List<int[]> coordinates = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i ++){
+            String[] s = br.readLine().split(" ");
+            coordinates.add(findArray(s));
+        }
+
+        int[][] result = coordinates.toArray(new int [1][n]);
+
+        Arrays.sort(result, (a, b) -> {
+            if(a[1] != b[1]){
+                return Integer.compare(a[1], b[1]);
+            } else {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        for(int i = 0 ; i < n ; i ++){
+            bw.write(result[i][0]+ " " + result[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static int[] findArray(String[] s) {
+        int x = Integer.parseInt(s[0]);
+        int y = Integer.parseInt(s[1]);
+        int[] coordinate = {x, y};
+        return coordinate;
+    }
+}
